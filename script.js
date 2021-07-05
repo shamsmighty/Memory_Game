8fc9601cc409add6281b173867b708aa318918fd
Suraj Gupta <suraj.gupta.16@mountblue.tech>
2021-07-06 00:34:53 +0530

    Logic to create div with backGif and frontImg on it

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
-const lowestScore = document.getElementById("bestScore");
-const score = document.getElementById("score");
 const gameContainer = document.getElementById("game");
-lowestScore.textContent = JSON.parse(localStorage.getItem("score"));
-let hasFirstClickedCard = false;
-let firstCard = null;
-let secondCard = null;
-score.innerHTML = 0;
-let noClicking = false;
-let cardsFlipped = 0;
-let totalScore = undefined;
+
 const COLORS = [
-	"red",
-	"blue",
-	"green",
-	"orange",
-	"purple",
-	"red",
-	"blue",
-	"green",
-	"orange",
-	"purple",
+	"1.gif",
+	"1.gif",
+	"2.gif",
+	"2.gif",
+	"3.gif",
+	"3.gif",
+	"4.gif",
+	"4.gif",
+	"5.gif",
+	"5.gif",
+	"6.gif",
+	"6.gif",
+	"7.gif",
+	"7.gif",
+	"8.gif",
+	"8.gif",
+	"9.gif",
+	"9.gif",
+	"10.gif",
+	"10.gif",
+	"11.gif",
+	"11.gif",
+	"12.gif",
+	"12.gif",
 ];
 
 // here is a helper function to shuffle an array
@@ -56,7 +61,17 @@ function createDivsForColors(colorArray) {
 		const newDiv = document.createElement("div");
 
 		// give it a class attribute for the value we are looping over
-		newDiv.classList.add(color);
+		newDiv.classList.add("box");
+
+		const frontImage = document.createElement("img");
+		frontImage.src = "./img/javascript-seeklogo.com.svg";
+		frontImage.classList.add("frontImage");
+
+		const backImage = document.createElement("img");
+		backImage.src = `./gifs/${color}`;
+		backImage.classList.add("backImage");
+		newDiv.appendChild(frontImage);
+		newDiv.appendChild(backImage);
 
 		// call a function handleCardClick when a div is clicked on
 		newDiv.addEventListener("click", handleCardClick);
@@ -69,72 +84,8 @@ function createDivsForColors(colorArray) {
 // TODO: Implement this function!
 function handleCardClick(event) {
 	// you can use event.target to see which element was clicked
-	//console.log(event.target);
-	//console.log(event.target.classList);
-
-	if (noClicking) return;
-
-	// if the card has been clicked, nothing will happen
-	if (event.target.classList.contains("firstCardColor")) return;
-
-	score.innerHTML = parseInt(score.innerHTML) + 1;
-	//change background color of card
-	let color = event.target.className;
-	event.target.style.backgroundColor = color;
-
-	// adding class name of firstCardColor to 1st and 2nd clicked card's
-	event.target.classList.add("firstCardColor");
-
-	//creating variable holding the total number of cards clicked (with class name firstCardColor)
-	let clickCount = document.querySelectorAll("div .firstCardColor").length;
-	//console.log(clickCount);
-
-	//define firstCard and secondCard
-	if (!hasFirstClickedCard) {
-		hasFirstClickedCard = true;
-		firstCard = event.target;
-	} else {
-		hasFirstClickedCard = false;
-		secondCard = event.target;
-	}
-
-	//  if two cards have clicked and the classes do not match.
-	//  Make no change to background-color
-	if (clickCount < 2) return;
-	if (clickCount == 2 && firstCard.className == secondCard.className) {
-		cardsFlipped += 2;
-		firstCard.classList.remove("firstCardColor");
-		secondCard.classList.remove("firstCardColor");
-		setTimeout(function () {
-			if (cardsFlipped === COLORS.length) {
-				// Storing the score in Local Storage only when the score is less then localStorage.
-				if (
-					parseInt(score.innerHTML) < JSON.parse(localStorage.getItem("score"))
-				)
-					localStorage.setItem("score", JSON.stringify(score.innerHTML));
-				alert("Game over!");
-			}
-			document.getElementById("buttonLogo").style.pointerEvents = "auto";
-		}, 1000);
-	} else {
-		noClicking = true;
-		setTimeout(() => {
-			firstCard.classList.remove("firstCardColor");
-			secondCard.classList.remove("firstCardColor");
-			firstCard.style.backgroundColor = "";
-			secondCard.style.backgroundColor = "";
-			noClicking = false;
-		}, 1000);
-	}
+	console.log("you clicked", event.target);
 }
 
 // when the DOM loads
-document
-	.getElementsByClassName("container")[0]
-	.addEventListener("click", () => {
-		createDivsForColors(shuffledColors);
-		document.getElementsByClassName("container")[0].style.display = "none";
-		document.getElementById("scoreBoard").style.display = "block";
-		document.getElementById("buttonLogo").style.display = "block";
-		document.getElementById("buttonLogo").style.pointerEvents = "none";
-	});
+createDivsForColors(shuffledColors);
